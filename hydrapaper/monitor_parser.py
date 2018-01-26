3ad871dcca0516f8bcb9e9099b54a5c0bdb397b0
Gabriele Musco <emaildigabry@gmail.com>
2018-01-26 11:01:22 +0100

    added support for monitors.xml version 1 as for #9

--- hydrapaper/monitor_parser.py
+++ hydrapaper/monitor_parser.py
@@ -21,6 +21,53 @@ class Monitor:
     def __repr__(self):
         return self.name
 
+def _monitor_parser_v1(doc):
+    try:
+        monitors = []
+        if type(json.loads(json.dumps(doc['monitors']['configuration']))) == list:
+            conf = doc['monitors']['configuration'][-1]
+        else:
+            conf = doc['monitors']['configuration']
+        index = 1
+        if type(conf['output']) == list:
+            for o in conf['output']:
+                monitors.append(Monitor(
+                    o['width'],
+                    o['height'],
+                    o['x'],
+                    o['y'],
+                    index,
+                    '{0} - {1}'.format(
+                        o['vendor'],
+                        o['@name']
+                    ),
+                    o['primary'] == 'yes'
+                ))
+                index += 1
+        else:
+            o = conf['output']
+            monitors.append(Monitor(
+                o['width'],
+                o['height'],
+                o['x'],
+                o['y'],
+                index,
+                '{0} - {1}'.format(
+                    o['vendor'],
+                    o['@name']
+                ),
+                o['primary'] == 'yes'
+            ))
+        return monitors
+
+    except Exception as e:
+        print('Error: error parsing {0}\n\nException:'.format(
+            path_to_monitors_xml
+        ))
+        print(e)
+        return None
+
+
 def build_monitors_from_dict(path_to_monitors_xml='{0}{1}'.format(HOME, MONITORS_XML_PATH)):
     """Builds a list of Monitor objects from a logicalmonitor dictionary list
     parsed from ~/.config/monitors.xml"""
@@ -31,6 +78,8 @@ def build_monitors_from_dict(path_to_monitors_xml='{0}{1}'.format(HOME, MONITORS
     try:
         with open(path_to_monitors_xml) as fd:
             doc = xmltodict.parse(fd.read())
+        if doc['monitors']['@version'] == 1: # Really expect version 2 in most cases
+            return _monitor_parser_v1(doc)
         if type(json.loads(json.dumps(doc['monitors']['configuration']))) == list:
             lm_list = doc['monitors']['configuration'][-1]['logicalmonitor']
         else:
@@ -54,19 +103,17 @@ def build_monitors_from_dict(path_to_monitors_xml='{0}{1}'.format(HOME, MONITORS
                 index += 1
         else:
             monitors.append(Monitor(
-                    lm_list['monitor']['mode']['width'],
-                    lm_list['monitor']['mode']['height'],
-                    lm_list['x'],
-                    lm_list['y'],
-                    index,
-                    '{0} - {1}'.format(
-                        lm_list['monitor']['monitorspec']['vendor'],
-                        lm_list['monitor']['monitorspec']['connector']
-                    ),
-                    ('primary' in lm_list)
-                ))
-        if not monitors[0].primary:
-            monitors[0], monitors[1] = monitors[1], monitors[0]
+                lm_list['monitor']['mode']['width'],
+                lm_list['monitor']['mode']['height'],
+                lm_list['x'],
+                lm_list['y'],
+                index,
+                '{0} - {1}'.format(
+                    lm_list['monitor']['monitorspec']['vendor'],
+                    lm_list['monitor']['monitorspec']['connector']
+                ),
+                ('primary' in lm_list)
+            ))
         return monitors
     except Exception as e:
         print('Error: error parsing {0}\n\nException:'.format(
